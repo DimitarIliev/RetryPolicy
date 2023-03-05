6cce985a28750c74374064eafc7e026761eb7eb8
Dimitar Iliev <diki_tazo@hotmail.com>
2023-03-05 21:15:12 +0100

    Add project files.

---
+++ RetryPolicy/Controllers/LocationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetryPolicy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LocationController : ControllerBase
+    {
+    }
+}
